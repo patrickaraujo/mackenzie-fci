1253c944f06607270e641db803b58f3ca19f407d
Patrick Araújo <patrick.araujo@students.ic.unicamp.br>
2026-02-23 15:17:42 -0300

    Update PiscinaeBaldes.java

--- LP 1/Aula2/PiscinaeBaldes.java
+++ LP 1/Aula2/PiscinaeBaldes.java
@@ -10,42 +10,55 @@ import static Aula1.UnionAndIntersection.printVector;
 import java.util.Scanner;
 
 /**
- *
+ * Jogo que determina um vencedor (Carlos ou Marcelo) com base no 
+ * número de trocas necessárias para ordenar um vetor gerado aleatoriamente.
  * @author Patrick
  */
+
 public class PiscinaeBaldes {
     
+    /**
+     * Aplica o algoritmo Bubble Sort no array e conta o número de trocas.
+     * * @param array O vetor desordenado.
+     * @return O número total de inversões (trocas) realizadas.
+     */
     public static int bubble(int[] array) {
         int cont = 0;
-        for (int fixo = 0; fixo < array.length; fixo++)
-            for (int i = 0; i < array.length-1; i++)
-                if (array[i] > array[i+1]){
+        for (int fixo = 0; fixo < array.length; fixo++) {
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
                     int aux = array[i];
-                    array[i] = array[i+1];
-                    array[i+1] = aux;
+                    array[i] = array[i + 1];
+                    array[i + 1] = aux;
                     cont++;
                 }
+            }
+        }
         return cont;
     }
-    
     
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         
-        System.out.println("Entre o número");
+        System.out.println("Entre o número de elementos do vetor:");
         int y;
-        do{
+        do {
             y = entrada.nextInt();
-        }while(y <= 0);
+        } while (y <= 0);
         
         int a[] = generateVector(y);
-        System.out.println("Vetor");
+        System.out.println("Vetor gerado:");
         printVector(a);
+        
         int x = bubble(a);
-        if(x % 2 == 0)
-            System.out.println("Carlos");
-        else
-            System.out.println("Marcelo");
-            
+        
+        // Determina o vencedor baseado na paridade das trocas
+        if (x % 2 == 0) {
+            System.out.println("Carlos (Número de trocas par: " + x + ")");
+        } else {
+            System.out.println("Marcelo (Número de trocas ímpar: " + x + ")");
+        }
+        
+        entrada.close();
     }
 }
