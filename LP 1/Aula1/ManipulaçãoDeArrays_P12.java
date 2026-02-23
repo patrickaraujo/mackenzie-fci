3d650a2bbcbf612b1c75df2eb6ec6575356e08b0
Patrick Araújo <patrick.araujo@students.ic.unicamp.br>
2026-02-23 15:47:34 -0300

    Update ManipulaçãoDeArrays_P12.java

--- LP 1/Aula1/ManipulaçãoDeArrays_P12.java
+++ LP 1/Aula1/ManipulaçãoDeArrays_P12.java
@@ -5,8 +5,6 @@
  */
 package Aula1;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -31,14 +29,17 @@ public class Problema12 {
     /**
      * Imprime os elementos do vetor formatados.
      */
-    public static void printVector(int v[]) {
-        for (int i = 0; i < v.length; i++) {
-            if (i == (v.length) - 2)
+    public static void printVector(int v[], int quantidadePreenchida) {
+        for (int i = 0; i < quantidadePreenchida; i++) {
+            if (i == quantidadePreenchida - 2)
                 System.out.print("Posição " + i + ": (" + v[i] + ") e ");
-            else if (i != (v.length) - 1)
+            else if (i != quantidadePreenchida - 1)
                 System.out.print("Posição " + i + ": (" + v[i] + "), ");
             else
                 System.out.print("Posição " + i + ": (" + v[i] + ")");
+        }
+        if (quantidadePreenchida == 0) {
+            System.out.print("Vetor vazio");
         }
         System.out.println();
     }
@@ -60,61 +61,70 @@ public class Problema12 {
      */
     public static int[] addposition(int[] v, int pos, int ele, int quantidadePreenchida) {
         if (v.length == quantidadePreenchida) {
-            addposition(v, pos, ele, quantidadePreenchida - 1);
-        } else {
-            ArrayList<Integer> fim = new ArrayList<>();
-            for (int i = pos; i < quantidadePreenchida; i++) {
-                fim.add(v[i]);
-            }
-            int z[] = converterParaArray(fim);
-            v[pos] = ele;
-            for (int j = 0; j < z.length; j++) {
-                v[j + (pos + 1)] = z[j];
-            }
+            System.out.println("Array cheio - não é possível adicionar");
+            return v;
         }
+        
+        // Desloca elementos para a direita a partir da posição
+        for (int i = quantidadePreenchida; i > pos; i--) {
+            v[i] = v[i - 1];
+        }
+        
+        // Insere o novo elemento
+        v[pos] = ele;
+        
         return v;
     }
     
     /**
      * Remove um elemento de uma posição específica.
      */
-    public static int[] removeposition(int[] v, int pos, int tamanhoMaximo) {
-        ArrayList<Integer> fim = new ArrayList<>();
-        for (int i = pos + 1; i < tamanhoMaximo; i++) {
-            fim.add(v[i]);
+    public static int[] removeposition(int[] v, int pos, int quantidadePreenchida) {
+        if (quantidadePreenchida == 0) {
+            System.out.println("Vetor vazio - não há o que remover");
+            return v;
         }
-        int z[] = converterParaArray(fim);
-        for (int j = 0; j < z.length; j++) {
-            v[j + pos] = z[j];
+        
+        // Desloca elementos para a esquerda a partir da posição
+        for (int i = pos; i < quantidadePreenchida - 1; i++) {
+            v[i] = v[i + 1];
         }
-        v[tamanhoMaximo - 1] = 0;
+        
+        // Limpa a última posição
+        v[quantidadePreenchida - 1] = 0;
+        
         return v;
     }
     
     /**
      * Remove todas as ocorrências de um elemento específico.
      */
-    public static int[] removeall(int[] v, int eleme, int tamanhoMaximo) {
-        int achou;
-        int i = 0;
-        do {
-            achou = lastIndexOf(v, eleme);
-            if (achou != -1) {
-                v = removeposition(v, achou, tamanhoMaximo);
-            } else if (i == 0 && achou == -1) {
-                System.out.println("Não há nada a ser removido");
+    public static int[] removeall(int[] v, int eleme, int quantidadePreenchida) {
+        boolean encontrou = false;
+        
+        // Percorre o array procurando o elemento
+        for (int i = 0; i < quantidadePreenchida; i++) {
+            if (v[i] == eleme) {
+                // Remove o elemento na posição i
+                v = removeposition(v, i, quantidadePreenchida);
+                quantidadePreenchida--;
+                encontrou = true;
+                i--; // Volta uma posição pois os elementos foram deslocados
             }
-            i++;
-        } while (achou != -1);
-
+        }
+        
+        if (!encontrou) {
+            System.out.println("Não há nada a ser removido");
+        }
+        
         return v;
     }
     
     /**
      * Verifica se um elemento existe no vetor e imprime o resultado.
      */
-    public static void check(int[] v, int elemen) {
-        int ultimo = lastIndexOf(v, elemen);
+    public static void check(int[] v, int elemen, int quantidadePreenchida) {
+        int ultimo = lastIndexOf(v, elemen, quantidadePreenchida);
         if (ultimo >= 0)
             System.out.println(elemen + " está no vetor, na posição: " + ultimo);
         else
@@ -124,12 +134,13 @@ public class Problema12 {
     /**
      * Verifica se existem dois números no array que, somados, resultam no valor informado.
      */
-    public static boolean printPairs(int sum, int[] array) {
-        for (int i = 0; i < array.length; i++) {
+    public static boolean printPairs(int sum, int[] array, int quantidadePreenchida) {
+        for (int i = 0; i < quantidadePreenchida; i++) {
             int first = array[i];
-            for (int j = i + 1; j < array.length; j++) {
+            for (int j = i + 1; j < quantidadePreenchida; j++) {
                 int second = array[j];
                 if ((first + second) == sum) {
+                    System.out.println("Pares encontrados: " + first + " + " + second + " = " + sum);
                     return true;
                 }
             }
@@ -140,23 +151,11 @@ public class Problema12 {
     /**
      * Encontra a última posição de um elemento no array.
      */
-    public static int lastIndexOf(int v[], int e) {
-        for (int i = (v.length - 1); i >= 0; i--) {
+    public static int lastIndexOf(int v[], int e, int quantidadePreenchida) {
+        for (int i = quantidadePreenchida - 1; i >= 0; i--) {
             if (v[i] == e) return i;
         }
         return -1;
-    }
-    
-    /**
-     * Converte um ArrayList de Integer para um array de int primitivo.
-     */
-    public static int[] converterParaArray(ArrayList<Integer> l) {
-        Integer g[] = l.toArray(new Integer[0]);
-        int v[] = new int[g.length];
-        for (int i = 0; i < g.length; i++) {
-            v[i] = g[i];
-        }
-        return v;
     }
     
     /**
@@ -177,10 +176,19 @@ public class Problema12 {
         do {
             System.out.println("Limite até 100");
             quan = entrada.nextInt();
-        } while (quan > 100);
+        } while (quan > 100 || quan <= 0);
         
-        int x[] = Arrays.copyOf(newArray(quan / 4), quan);
-        printVector(x);
+        // Inicializa o array com 1/4 dos elementos preenchidos
+        int x[] = new int[quan];
+        int inicialPreenchido = quan / 4;
+        for (int i = 0; i < inicialPreenchido; i++) {
+            do {
+                x[i] = (int) (Math.random() * 100);
+            } while (x[i] == 0);
+        }
+        
+        int quantidadePreenchida = inicialPreenchido;
+        printVector(x, quantidadePreenchida);
         
         int operacao;
         do {
@@ -191,73 +199,119 @@ public class Problema12 {
                 case 1:
                     System.out.println("Entre com o elemento:");
                     int elem;
-                    do { elem = entrada.nextInt(); } while (elem == 0);
-                    x = addend(x, elem, contarElementosNaoNulos(x));
-                    printVector(x);
-                    break;
-                case 2:
-                    System.out.println("Em qual posição?");
-                    int pos1;
-                    if (x[0] != 0) {
-                        do {
-                            System.out.println("Posição: ");
-                            pos1 = entrada.nextInt();
-                        } while (pos1 > contarElementosNaoNulos(x) || pos1 > quan - 1);
+                    do { 
+                        elem = entrada.nextInt(); 
+                    } while (elem == 0);
+                    
+                    if (quantidadePreenchida < x.length) {
+                        x = addend(x, elem, quantidadePreenchida);
+                        quantidadePreenchida++;
+                        printVector(x, quantidadePreenchida);
                     } else {
-                        do {
-                            System.out.println("Digite 0(Zero)");
-                            pos1 = entrada.nextInt();
-                        } while (pos1 != 0);
+                        System.out.println("Array cheio - não é possível adicionar");
                     }
+                    break;
+                    
+                case 2:
+                    if (quantidadePreenchida == 0) {
+                        System.out.println("Vetor vazio. Adicione elementos primeiro.");
+                        break;
+                    }
+                    
+                    System.out.println("Em qual posição? (0 a " + quantidadePreenchida + ")");
+                    int pos1;
+                    do {
+                        pos1 = entrada.nextInt();
+                    } while (pos1 < 0 || pos1 > quantidadePreenchida || pos1 >= x.length);
+                    
                     System.out.println("Entre com o elemento:");
                     int elemento;
-                    do { elemento = entrada.nextInt(); } while (elemento == 0);
-                    x = addposition(x, pos1, elemento, contarElementosNaoNulos(x));
-                    printVector(x);
+                    do { 
+                        elemento = entrada.nextInt(); 
+                    } while (elemento == 0);
+                    
+                    if (quantidadePreenchida < x.length) {
+                        x = addposition(x, pos1, elemento, quantidadePreenchida);
+                        quantidadePreenchida++;
+                        printVector(x, quantidadePreenchida);
+                    } else {
+                        System.out.println("Array cheio - não é possível adicionar");
+                    }
                     break;
+                    
                 case 3:
-                    System.out.println("Em qual posição?");
-                    int pos2;
-                    if (contarElementosNaoNulos(x) == 0) {
+                    if (quantidadePreenchida == 0) {
                         System.out.println("Não há nada a remover");
                         break;
-                    } else {
-                        do {
-                            System.out.println("Posição Limite: " + (contarElementosNaoNulos(x) - 1));
-                            pos2 = entrada.nextInt();
-                        } while (pos2 >= contarElementosNaoNulos(x));
                     }
-                    x = removeposition(x, pos2, x.length);
-                    printVector(x);
+                    
+                    System.out.println("Em qual posição? (0 a " + (quantidadePreenchida - 1) + ")");
+                    int pos2;
+                    do {
+                        pos2 = entrada.nextInt();
+                    } while (pos2 < 0 || pos2 >= quantidadePreenchida);
+                    
+                    x = removeposition(x, pos2, quantidadePreenchida);
+                    quantidadePreenchida--;
+                    printVector(x, quantidadePreenchida);
                     break;
+                    
                 case 4:
+                    if (quantidadePreenchida == 0) {
+                        System.out.println("Não há nada a remover");
+                        break;
+                    }
+                    
                     System.out.println("Qual elemento?");
                     int eleme = entrada.nextInt();
-                    if (eleme == 0 || contarElementosNaoNulos(x) == 0) {
-                        System.out.println("Não há nada a remover");
+                    
+                    if (eleme == 0) {
+                        System.out.println("Elemento inválido");
                         break;
                     }
-                    x = removeall(x, eleme, contarElementosNaoNulos(x));
-                    printVector(x);
+                    
+                    int antes = quantidadePreenchida;
+                    x = removeall(x, eleme, quantidadePreenchida);
+                    quantidadePreenchida = contarElementosNaoNulos(x);
+                    
+                    if (antes == quantidadePreenchida) {
+                        System.out.println("Elemento não encontrado no vetor");
+                    } else {
+                        printVector(x, quantidadePreenchida);
+                    }
                     break;
+                    
                 case 5:
                     System.out.println("Qual elemento?");
                     int elemen = entrada.nextInt();
-                    check(x, elemen);
+                    check(x, elemen, quantidadePreenchida);
                     break;
+                    
                 case 6:
-                    System.out.println("Qual elemento?");
+                    if (quantidadePreenchida < 2) {
+                        System.out.println("Precisa de pelo menos 2 elementos no vetor");
+                        break;
+                    }
+                    
+                    System.out.println("Qual valor da soma?");
                     int element = entrada.nextInt();
-                    if (printPairs(element, x))
-                        System.out.println("Há elementos");
+                    
+                    if (printPairs(element, x, quantidadePreenchida))
+                        System.out.println("Há elementos que somam " + element);
                     else
-                        System.out.println("Não há elementos");
+                        System.out.println("Não há elementos que somam " + element);
                     break;
-                default:
+                    
+                case 7:
                     System.out.println("Fim da execução");
                     break;
+                    
+                default:
+                    System.out.println("Opção inválida. Escolha de 1 a 7.");
+                    break;
             }
-        } while(operacao > 0 && operacao < 7);
+        } while(operacao != 7);
+        
         entrada.close();
     }
 }
