18222501e8f70672b0537c8a7f417fd0c90dba3e
Patrick Araújo <patrick.araujo@students.ic.unicamp.br>
2026-02-23 15:19:08 -0300

    Update VectorOperations.java

--- LP 1/Aula2/VectorOperations.java
+++ LP 1/Aula2/VectorOperations.java
@@ -9,74 +9,96 @@ import static Aula1.UnionAndIntersection.generateVector;
 import static Aula1.UnionAndIntersection.printVector;
 
 /**
- *
+ * Fornece métodos estáticos base para algoritmos clássicos
+ * de ordenação e busca em vetores primitivos.
  * @author 41318862
  */
 public class VectorOperations {
-    public static int linearSearch (int v[], int e){
-        for (int i = 0; i < v.length; i++)
-            if(v[i] == e)
-                return i;
-        return -1;
-    }
     
-    public static int linearSearchLast (int v[], int e){
-        for (int i = (v.length-1); i >= 0; i--)
-            if (v[i] == e)
-                return i;
-        return -1;
-    }
-    
-    private static int binarySearch(int[] array, int valor) {
-        int inicio = 0;
-        int fim = array.length-1;
-        
-        while(inicio <= fim) {
-            int meio = (inicio+fim)/2;
-            
-            if(array[meio] == valor)
-                return meio;
-            
-            if(valor > array[meio])
-            inicio = meio+1;
-            
-            else
-                fim = meio-1;
-
+    /**
+     * Realiza uma busca linear sequencial e retorna a primeira ocorrência do elemento.
+     */
+    public static int linearSearch(int v[], int e) {
+        for (int i = 0; i < v.length; i++) {
+            if (v[i] == e) return i;
         }
         return -1;
     }
     
-    public static void bubbleSort(int v[]){
-        for(int i = 0; i < (v.length - 1); i++)
-            for(int j = 0; j < (v.length - (1 + i)); j++)
-                if(v[j] > v [j + 1]){
+    /**
+     * Realiza uma busca linear invertida (do final para o começo) para achar
+     * a última ocorrência de um elemento no vetor.
+     */
+    public static int linearSearchLast(int v[], int e) {
+        for (int i = (v.length - 1); i >= 0; i--) {
+            if (v[i] == e) return i;
+        }
+        return -1;
+    }
+    
+    /**
+     * Executa o algoritmo de Busca Binária.
+     * IMPORTANTE: O array já deve estar ordenado antes de invocar este método.
+     */
+    public static int binarySearch(int[] array, int valor) {
+        int inicio = 0;
+        int fim = array.length - 1;
+        
+        while (inicio <= fim) {
+            int meio = (inicio + fim) / 2;
+            
+            if (array[meio] == valor) return meio;
+            
+            if (valor > array[meio]) {
+                inicio = meio + 1;
+            } else {
+                fim = meio - 1;
+            }
+        }
+        return -1;
+    }
+    
+    /**
+     * Algoritmo de ordenação Bubble Sort.
+     */
+    public static void bubbleSort(int v[]) {
+        for (int i = 0; i < (v.length - 1); i++) {
+            for (int j = 0; j < (v.length - (1 + i)); j++) {
+                if (v[j] > v[j + 1]) {
                     int aux = v[j];
                     v[j] = v[j + 1];
                     v[j + 1] = aux;
                 }
+            }
+        }
     }
     
     public static void main(String[] args) {
-        System.out.println("b)");
+        System.out.println("Teste B - Vetor Aleatório e Busca Linear Invertida");
         int A[] = generateVector(10);
-        System.out.println("Vetor");
+        System.out.println("Vetor Original:");
         printVector(A);
+        
         bubbleSort(A);
-        System.out.println("Vetor ordenado");
+        System.out.println("Vetor Ordenado:");
         printVector(A);
+        
         int res = linearSearchLast(A, 2);
-        if(res != -1)
-            System.out.println("Última ocorrência\nPosição: "+res);
+        if (res != -1)
+            System.out.println("Última ocorrência do '2'\nPosição: " + res);
         else
             System.out.println("Não achou 2 no vetor. :(");
         
-        System.out.println("\n\na)");
+        System.out.println("\n-----------------------------------\n");
+        
+        System.out.println("Teste A - Vetor Estático e Busca Linear Invertida");
         int B[] = {8, 7, 4, 21, 54, 28, 4, 89, 8};
+        System.out.println("Vetor Original B:");
         printVector(B);
+        
         res = linearSearchLast(B, 4);
-        if(res != -1)
-            System.out.println("Última ocorrência\nPosição: "+res);
+        if (res != -1)
+            System.out.println("Última ocorrência do '4'\nPosição: " + res);
         else
             System.out.println("Não achou 4 no vetor. :(");
     }
